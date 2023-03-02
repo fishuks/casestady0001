fd746368ba22be1677f3e52f52903f095060f367
fishukaa <103937450+fishuks@users.noreply.github.com>
2023-03-02 11:16:31 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+'''
+Title
+Group:
+Fishchukova Sofia
+Thvikh Viktoria
+'''
+
+def trengle (x,y,a,b,angel,color):
+    '''
+
+    :param x:
+    :param y:
+    :param a:
+    :param b: 
+    :param angel:
+    :param color:
+    :return:
+    '''
+    pass
+def draw_dog():
+    pass
+def drow_cat():
+    pass
+def main():
+    '''
+
+    :return:
+    '''
+    draw_dog()
+    drow_cat()
+
+if __name__=='__main__':
+    main()
